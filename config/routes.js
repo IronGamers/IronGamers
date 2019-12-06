96e14eef75d7752db1ae8c5f9f049422598d13a2
unknown <rober.araya.gil@gmail.com>
2019-12-06 22:43:26 +0100

    include auth

--- config/routes.js
+++ config/routes.js
@@ -4,17 +4,20 @@ const baseController = require('../controllers/base.controller')
 const usersController = require('../controllers/users.controller')
 const genderController = require('../controllers/gender.controller')
 const gameController = require('../controllers/game.controller')
+const authMiddelware = require('../middlewares/auth.middleware')
 const multer = require('multer');
-const upload = multer({ dest: './public/uploads/' });
+const upload = multer({
+    dest: './public/uploads/'
+});
 
 // USER ROUTES
 
-router.get('/', baseController.home)
-router.get('/user/login', usersController.login)
-router.post('/login', usersController.doLogin)
-router.get('/user/new', usersController.new)
-router.post('/user/new', usersController.create)
-router.post('/user/logout', usersController.logout)
+router.get('/', authMiddelware.isAuthenticated, baseController.home)
+router.get('/user/login', authMiddelware.isNotAuthenticated, usersController.login)
+router.post('/login', authMiddelware.isNotAuthenticated, usersController.doLogin)
+router.get('/user/new', authMiddelware.isNotAuthenticated, usersController.new)
+router.post('/user/new', authMiddelware.isNotAuthenticated, usersController.create)
+router.post('/user/logout', authMiddelware.isAuthenticated, usersController.logout)
 
 
 // GENDER ROUTES (PENDIENTE PONER MIDDLEWARE)
